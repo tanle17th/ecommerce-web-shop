a829710f9becdbf14ba0853f9e0ba9ec57857076
VanPhucTanLe <le000212@algonquinlive.com>
2022-01-18 01:10:57 -0500

    Initial commit

---
+++ ecommerce-web-shop/src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Grid from '@material-ui/core'
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}></Grid>
+    </main>
+  )
+}
+
+export default Products
